30f90efe42ec2d2adedc4ad7b0ee0b793a39f224
KwonYI <kkwinwin95@naver.com>
2021-04-26 17:15:03 +0900

    feat : 구매 기록 조회 S04P31A103-95

---
+++ backend/a103/src/main/java/com/a103/apiServer/buy/BuyController.java
@@ -0,0 +1,56 @@
+package com.a103.apiServer.buy;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.a103.apiServer.model.Buy;
+
+@RestController
+@RequestMapping("/buy")
+public class BuyController {
+
+	@Autowired
+	BuyDao buyDao;
+
+	public static final Logger logger = LoggerFactory.getLogger(BuyController.class);
+
+	@GetMapping(value = "list/{member_id}")
+	public ResponseEntity<Map<String, Object>> getBuyList(@PathVariable("member_id") int memberId) {
+		ResponseEntity<Map<String, Object>> entity = null;
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			List<Buy> buyList = buyDao.findListBuyByMemberId(memberId);
+			
+			if (buyList.size() != 0){
+	            result.put("success", "success");
+	            result.put("data", buyList);
+	            entity = new ResponseEntity<>(result, HttpStatus.OK);
+	            
+	        }
+	        else {
+	            result.put("success", "fail");
+	            entity = new ResponseEntity<>(result, HttpStatus.OK);
+	        }
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("success", "error");
+			entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return entity;
+	}
+}
---
+++ backend/a103/src/main/java/com/a103/apiServer/model/Buy.java
@@ -0,0 +1,46 @@
+package com.a103.apiServer.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Buy {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "buy_date")
+	private LocalDate buyDate;
+
+	private int price;
+
+	private int count;
+
+//	@ManyToOne
+//	@JoinColumn(name = "member_id")
+//	private Member member;
+	
+	@Column(name = "member_id")
+	private long memberId;
+
+//	@ManyToOne
+//	@JoinColumn(name = "product_id")
+//	private Product product;
+	
+	@Column(name = "product_id")
+	private long productId;
+}
