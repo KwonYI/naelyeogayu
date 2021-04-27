69c71ebe239451f52b20a4f91730f8c092c05f38
김명규 <figtoo@naver.com>
2021-04-27 12:24:54 +0900

    #S04P31A103-94 회원정보 조회
    # model 추가
    Member 추가
    
    # 회원정보 조회 구현
    1. 회원정보가 없을 경우
    - success : "fail"
    
    2. 회원정보가 있을 경우
    - success : "success"
    - data : Member

---
+++ backend/a103/src/main/java/com/a103/apiServer/member/MemberController.java
@@ -0,0 +1,54 @@
+package com.a103.apiServer.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.a103.apiServer.model.Member;
+
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+	
+	@Autowired
+	MemberDao memberDao;
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@GetMapping(value = "profile/{email}")
+	public ResponseEntity getMember(@PathVariable("email") String email) {
+		ResponseEntity entity = null;
+		Map result = new HashMap<>();
+		
+		try {
+			Member member = memberDao.findMemberByEmail(email);
+			
+			if(member != null) {
+				result.put("success","success");
+				result.put("data", member);
+				entity = new ResponseEntity<>(result, HttpStatus.OK);
+			}else {
+				result.put("success","fail");
+				entity = new ResponseEntity<>(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			logger.error("error", e);
+			result.put("success","error");
+			entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+	}
+	
+}
---
+++ backend/a103/src/main/java/com/a103/apiServer/member/MemberDao.java
@@ -0,0 +1,11 @@
+package com.a103.apiServer.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.a103.apiServer.model.Member;
+
+@Repository
+public interface MemberDao extends JpaRepository<Member, Long>{
+	Member findMemberByEmail(String email);
+}
