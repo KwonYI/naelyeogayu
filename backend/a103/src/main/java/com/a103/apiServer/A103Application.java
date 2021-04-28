4e065cbbdb8b0fc5291ed20fa8f975965c353510
김명규 <figtoo@naver.com>
2021-04-29 04:27:11 +0900

    #S04P31A103-118 feat : 회원탈퇴 기능 구현

--- backend/a103/src/main/java/com/a103/apiServer/A103Application.java
+++ backend/a103/src/main/java/com/a103/apiServer/A103Application.java
@@ -28,6 +28,7 @@ public class A103Application implements WebMvcConfigurer{
 	  		  "/member/login",
 	  		  "/member/social",
 	  		  "/member/password",
+	  		  "/member/delete",
 	   		  "/member/signup",
 	   		  "/member/certify"));// 적용 제외 경로
 	}
