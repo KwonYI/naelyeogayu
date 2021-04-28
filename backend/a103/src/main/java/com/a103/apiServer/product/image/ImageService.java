a8f7d2455a31f6f9e2fcd7908e020ed6f2a50e2d
권영일 <kkwinwin95@naver.com>
2021-04-28 02:19:25 +0000

    Update ImageService.java

--- backend/a103/src/main/java/com/a103/apiServer/product/image/ImageService.java
+++ backend/a103/src/main/java/com/a103/apiServer/product/image/ImageService.java
@@ -4,5 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
 	public void setS3Client() throws Exception;
+	
 	public String upload(MultipartFile file) throws Exception;
 }
