89f3616d2516836ed038c2807d018cffc7ec249e
KwonYI <kkwinwin95@naver.com>
2021-04-29 13:45:39 +0900

    #S04P31A103-108 feat : 유통기한 임박 상품 조회

--- backend/a103/src/main/java/com/a103/apiServer/product/ProductController.java
+++ backend/a103/src/main/java/com/a103/apiServer/product/ProductController.java
@@ -1,14 +1,15 @@
 package com.a103.apiServer.product;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,4 +43,30 @@ public class ProductController {
 
 		return entity;
 	}
+
+	@GetMapping(value = "/expire")
+	public ResponseEntity getExpireProductList() {
+		ResponseEntity entity = null;
+		Map result = new HashMap<>();
+
+		try {
+			List<Product> productList = productDao.findListProductByCategory(1);
+
+			if (productList.size() != 0) {
+				result.put("success", "success");
+				result.put("data", productList);
+				entity = new ResponseEntity<>(result, HttpStatus.OK);
+			} else {
+				result.put("success", "fail");
+				entity = new ResponseEntity<>(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			logger.error("error", e);
+			result.put("success", "error");
+			entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return entity;
+	}
+
 }
--- backend/a103/src/main/java/com/a103/apiServer/product/ProductDao.java
+++ backend/a103/src/main/java/com/a103/apiServer/product/ProductDao.java
@@ -1,11 +1,15 @@
 package com.a103.apiServer.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.a103.apiServer.model.Product;
 
 @Repository
-public interface ProductDao extends JpaRepository<Product, Long>{
+public interface ProductDao extends JpaRepository<Product, Long> {
+	
 	Product findProductById(long id);
+	List<Product> findListProductByCategory(int category);
 }
