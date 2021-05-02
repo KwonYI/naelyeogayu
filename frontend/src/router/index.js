697580be178bd707d81205d699bd08c15071a4d0
송지헌 <wlgjs0817@naver.com>
2021-05-02 08:19:13 +0000

    #S04P31A103-71 feat : 상품 등록

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/Home.vue";
 import Join from "../views/user/Join.vue";
 import Login from "../views/user/Login.vue";
 import About from "../views/About.vue";
+import Upload from "../views/product/Upload.vue";
 import SocialJoin from "../views/user/SocialJoin.vue";
 
 Vue.use(VueRouter);
@@ -33,6 +34,11 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/upload",
+    name: "Upload",
+    component: Upload,
   },
 ];
 
---
+++ frontend/src/views/product/Upload.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div>
+      <h1>상품 등록</h1>
+    </div>
+    <v-divider width="1280" style="margin: 0 auto"></v-divider>
+    <terms v-if="this.step === 1" @agreeall="check" />
+    <forms v-if="this.step === 2" @back="backpage" @upload="submit" />
+  </div>
+</template>
+
+<script>
+import terms from "@/components/product/upload/terms.vue";
+import forms from "@/components/product/upload/Uploadform.vue";
+export default {
+  components: {
+    terms,
+    forms,
+  },
+  created() {
+    const id = this.getUserId;
+    if (id.length == 0) {
+      alert("로그인 먼저해주세요");
+      this.$router.push({ name: "Login" });
+    }
+  },
+  data: function () {
+    return {
+      step: 1,
+    };
+  },
+  methods: {
+    check(flag) {
+      if (flag) this.step++;
+    },
+    backpage(flag) {
+      if (flag) this.step--;
+    },
+    submit(product) {
+      product.sellerId = Number(this.getUserId);
+      console.log(product);
+      this.$store.dispatch("productStore/upload", product);
+    },
+  },
+  computed: {
+    getUserId() {
+      return this.$store.getters["userStore/id"];
+    },
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "NEXON Lv1 Gothic OTF Bold";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF Bold.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+div h1 {
+  padding: 10px;
+}
+
+div h1 {
+  text-align: center;
+  font-family: "NEXON Lv1 Gothic OTF Bold";
+}
+</style>
