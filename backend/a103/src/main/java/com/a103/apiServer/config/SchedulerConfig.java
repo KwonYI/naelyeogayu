de97e498f0a1671ca1501992803cb4d16e1fcddd
하태민 <haruto4380@gmail.com>
2021-05-03 05:56:56 +0000

    Update SchedulerConfig.java

--- backend/a103/src/main/java/com/a103/apiServer/config/SchedulerConfig.java
+++ backend/a103/src/main/java/com/a103/apiServer/config/SchedulerConfig.java
@@ -2,7 +2,6 @@ package com.a103.apiServer.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
