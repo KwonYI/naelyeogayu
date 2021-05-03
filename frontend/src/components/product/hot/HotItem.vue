d9c755c7e9e14a00f8cc93e363a4665a598c0cc2
chunawoos <77089164+chunawoos@users.noreply.github.com>
2021-05-03 11:27:24 +0900

    #S04P31A103-147 feat : 리퍼브상품 레이아웃 구현

---
+++ frontend/src/components/product/hot/HotItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="hot">
+    <v-sheet class="mx-auto" elevation="8" max-width="1280" min-height="400">
+      <div class="hotTitle">HOT 상품</div>
+      <v-slide-group class="hotslide" active-class="success" show-arrows>
+        <v-slide-item v-for="n in 6" :key="n">
+          <hotCard></hotCard>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import hotCard from "@/components/product/hot/HotCard.vue";
+
+export default {
+  components: {
+    hotCard,
+  },
+};
+</script>
+
+<style>
+.hotTitle {
+  font-size: 40px;
+  margin-left: 15px;
+}
+</style>
