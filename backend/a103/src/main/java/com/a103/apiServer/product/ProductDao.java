b7d178a622bf8fba979767ca0af8d285c603cdfb
KwonYI <kkwinwin95@naver.com>
2021-05-04 11:22:38 +0900

    S04P31A103-159 feat : 상품 판매 목록 조회

--- backend/a103/src/main/java/com/a103/apiServer/product/ProductDao.java
+++ backend/a103/src/main/java/com/a103/apiServer/product/ProductDao.java
@@ -12,32 +12,34 @@ import com.a103.apiServer.model.Product;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Long> {
-	
-	Product findProductById(long id);
-	long countByCategoryAndStatus(int category, int status);
 
-	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	Product findProductById(long id);
+	long countByCategory(int category);
+	long countByCategoryAndStatus(int category, int status);
+	List<Product> findListProductBySellerId(int memberId);
+
+	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category ORDER BY STATUS LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByCategory(@Param("category") int category, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT * FROM product WHERE START_DATE >= :date LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	@Query(value = "SELECT * FROM product WHERE START_DATE >= :date ORDER BY STATUS LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByStartDate(@Param("date") LocalDate date, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT * FROM product WHERE NAME Like :word LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	@Query(value = "SELECT * FROM product WHERE NAME Like :word ORDER BY STATUS LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByNameContaining(@Param("word") String word, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT * FROM product WHERE DESCRIPT Like :word LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	@Query(value = "SELECT * FROM product WHERE DESCRIPT Like :word ORDER BY STATUS LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByDescriptContaining(@Param("word") String word, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT P.* FROM product P LEFT JOIN watchlog W ON P.ID = W.PRODUCT_ID GROUP BY P.ID HAVING COUNT(W.ID) > 0 ORDER BY COUNT(P.ID) DESC", nativeQuery = true)
+	@Query(value = "SELECT P.* FROM product P LEFT JOIN watchlog W ON P.ID = W.PRODUCT_ID WHERE STATUS = 0 GROUP BY P.ID HAVING COUNT(W.ID) > 0 ORDER BY COUNT(P.ID) DESC", nativeQuery = true)
 	List<Product> findListProductOrderByWatchCount();
-	
-	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category ORDER BY END_DATE ASC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+
+	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category ORDER BY STATUS AND END_DATE ASC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByCategoryOrderByEndDateAsc(@Param("category") int category, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category ORDER BY END_DATE DESC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	@Query(value = "SELECT * FROM product WHERE CATEGORY = :category ORDER BY STATUS AND END_DATE DESC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByCategoryOrderByEndDateDesc(@Param("category") int category, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
-	@Query(value = "SELECT P.* FROM product P LEFT JOIN watchlog W ON P.ID = W.PRODUCT_ID WHERE CATEGORY = :category GROUP BY P.ID ORDER BY COUNT(P.ID) DESC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
+	@Query(value = "SELECT P.* FROM product P LEFT JOIN watchlog W ON P.ID = W.PRODUCT_ID WHERE CATEGORY = :category GROUP BY P.ID ORDER BY STATUS AND COUNT(P.ID) DESC LIMIT :limit, :CONTENT_CNT", nativeQuery = true)
 	List<Product> findListProductByCategoryOrderByWatchCount(@Param("category") int category, @Param("limit") int limit, @Param("CONTENT_CNT") int contentCnt);
 
 }
