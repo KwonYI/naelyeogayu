e4371b83c73ce8c380a6cfbffb23cb0e28fa6b3c
wlgjs0817 <wlgjs0817@naver.com>
2021-05-06 15:24:10 +0900

    S04P31A103-145 못난이 농산물, 리퍼브 상품 리스트 조회

---
+++ frontend/src/components/product/upload/Terms.vue
@@ -0,0 +1,271 @@
+<template>
+  <div id="productUpload">
+    <div id="terms">
+      <h2>이용 약관</h2>
+      <v-row>
+        <div class="title"><p>경매 이용 약관</p></div>
+        <div class="title"><p>개인 정보 동의 약관</p></div>
+      </v-row>
+      <v-row>
+        <div class="term" id="auction">
+          <p style="font-weight: bold; text-align: center">
+            본 약관 조항을 주의 깊게 검토하여 주시기 바랍니다.<br />
+            내려가유 온라인 경매 및 매매계약은 아래 의거하여 진행되며,<br />
+            귀하는 경매 또는 계약에 참여함으로써 본 약관에 구속됨에 동의하신
+            것으로 간주됩니다.
+          </p>
+          <h5>1. 목적</h5>
+          <p>
+            본 경매 약관과 관련 경매도록 등에 포함된 유의사항은 다음의 사항을
+            규율함을 목적으로 한다.<br />
+            1.1 내려가유가 홈페이지상에서 실시하는 온라인 경매의 절차와 제반기준
+            및 관련자들의 권리・의무<br />
+            1.2 내려가유가 홈페이지상에서 실시하는 경매 이외의 방식에 의한
+            매매계약의 절차와 제반기준 및 관련자들의 권리・의무
+          </p>
+          <br />
+          <h5>2. 정의</h5>
+          <p>
+            2.1 "경매"라 함은 경매약관과 경매도록 등에 따라 내려가유가 실시하는
+            하향식 경매를 의미한다.<br />
+            2.2 “물품”이라 함은 농산품,리퍼브 상품 등 경매 및 일반적인 형태의
+            매매계약의 대상이 될 수 있는 모든 형태의 물품 또는 권리를 의미하고
+            “경매 물품”이라 함은 내려가유 경매에 출품되거나 경매 전 또는 후
+            세일의 대상이 되는 모든 물품을 의미한다.<br />
+            2.3 "경매마감일"이라 함은, 당사경매와 관련하여, 경매물품의
+            경매기간이 종료되는 일자를 의미한다.<br />
+            2.4 "경매기간"이라 함은, 내려가유 경매가 실시되는 일자 또는 기간을
+            의미한다.<br />
+            2.5 "구매자"라 함은, 경매물품에 관하여, 할인유에 의해 할인된 경매
+            물품의 가격으로서 경매 약관에 따라 해당 경매물품의 최종 경매
+            마감시간전에 내려가유에 의해 구매한 구매자를 의미한다.
+          </p>
+          <br />
+          <h5>3. 상품 등록</h5>
+          <p>
+            경매에 처음 등록하는 희망자를 포함한 등록 희망자는 관련 내려가유
+            등에 따라 위 경매등에 명시된 해당 상품등록신청서와 이에 명시되어
+            있는 서류를 작성 및 제출함으로써 상품 등록을 한다.<br />
+            구매 희망자의 경매 참가 등록과 경매 신청은 내려가유 등에 기재된 바에
+            따라 실시된다. 내려가유는 경매 상품 등록과 입찰신청, 구매를 처리하기
+            위하여 합리적인 노력을 기울인다.
+          </p>
+          <br />
+          <h5>4. 정보 보호 관련</h5>
+          <p>
+            17.1 응찰 희망자, 구매자는 경매에서의 응찰 및 경매물품의 구매와
+            관련하여 개인 정보를 제공할 것이 요구될 수 있고, 본 경매약관에
+            동의함으로써 그 개인정보의 제공 및 이용에 동의한것으로 본다. 모든
+            개인 정보는 다음의 목적을 위하여 이용될 수 있다.<br />
+            (i) 내려가유 관리와 경영 및 서비스의 홍보와 제공에 관한 모든 문제<br />
+            (ii) 내려가유의 서비스 및 등록 상품 홍보 목적 <br />
+            17.2 모든 개인 정보는 비공개로 보유되며, 구매자, 판매자, 기타 대리인
+            등은 그에 동의한 것으로 본다.<br />
+          </p>
+        </div>
+        <div class="term" id="userinfo">
+          <p style="font-weight: bold; text-align: center">
+            내려가유는 이용자의 ‘동의를 기반으로 개인정보를 수집·이용 및
+            제공’하고 있으며,<br />
+            ‘이용자의 권리 (개인정보 자기결정권)를 적극적으로 보장’합니다.
+          </p>
+          <br />
+          <h5>1. 개인정보의 수집 및 이용목적</h5>
+          <p>
+            본 서비스는 수집한 개인정보를 다음의 목적을 위해 활용합니다.<br />
+            1.1 서비스 제공에 관한 경매 이행 및 구매 및 요금 결제, 물품배송 또는
+            청구지 등 발송, 금융거래 본인 인증 및 서비스<br />
+            1.2 회원 관리 회원제 서비스 이용에 따른 본인확인, 개인 식별,
+            불량회원의 부정 이용 방지, 가입 의사 확인, 고지사항 전달<br />
+            1.3 마케팅 및 광고에 활용 신규 서비스(제품) 개발 및 특화, 이벤트 등
+            광고성 정보 전달, 인구통계학적 특성에 따른 서비스 제공 및 광고 게재,
+            접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계<br />
+          </p>
+          <br />
+          <h5>2. 수집하는 개인정보 항목</h5>
+          <p>
+            본 서비스는 회원가입, 상품 등록을 위해 아래와 같은 개인정보를
+            수집하고 있습니다.<br />
+            2.1 수집항목<br />
+            이용자의 식별을 위한 필수 정보: 이름, 아이디(ID)<br />
+            등록 상품을 위한 필수 정보 : 상품명, 상품 이미지, 상품 설명 경매<br />
+            진행을 위한 필수 정보 : 카테고리, 수량, 최고설정가, 최저설정가,
+            경매시작일, 경매마감일
+          </p>
+          <br />
+          <h5>3. 개인정보의 보유 및 이용기간</h5>
+          <p>
+            원칙적으로, 개인정보 수집 및 이용목적이 달성된 후에는 해당 정보를
+            지체 없이 파기합니다.<br />
+            또한, 개인정보는 회원관리, 서비스 제공·개선, 신규 서비스 개발 등을
+            위해 이용합니다.<br />
+            회원 가입 시 또는 서비스 이용 과정에서 홈페이지를 통한 서비스 제공을
+            위해 필요한 최소한의 개인정보를 수집하고 있습니다.
+          </p>
+        </div>
+      </v-row>
+    </div>
+    <div id="agreement">
+      <v-row class="term1">
+        <div class="agree">
+          <input
+            type="checkbox"
+            id="agree1"
+            v-model="agree1"
+            @click="check(1)"
+          />
+          <label for="agree1"> 경매 이용 약관에 동의합니다.</label>
+        </div>
+        <div class="agree">
+          <input
+            type="checkbox"
+            id="agree2"
+            v-model="agree2"
+            @click="check(2)"
+          />
+          <label for="agree2"> 개인 정보 이용 약관에 동의합니다.</label>
+        </div>
+      </v-row>
+      <v-row class="all">
+        <div class="agree">
+          <input
+            type="checkbox"
+            id="agreeAll"
+            v-model="agreeall"
+            @click="checkAll($event.target.checked)"
+          />
+          <label for="agreeAll"> 해당 약관에 대하여 전체 동의합니다.</label>
+        </div>
+      </v-row>
+      <v-row class="all">
+        <div class="btn">
+          <v-btn
+            v-if="this.agreeall"
+            outlined
+            black
+            style="font-family: 'NEXON Lv1 Gothic OTF'"
+            @click="nextpage()"
+            >다음<v-icon right>mdi-chevron-right</v-icon></v-btn
+          >
+          <v-btn
+            v-else
+            outlined
+            black
+            disabled
+            style="font-family: 'NEXON Lv1 Gothic OTF'"
+            >다음<v-icon right>mdi-chevron-right</v-icon></v-btn
+          >
+        </div>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      agree1: false,
+      agree2: false,
+      agreeall: false,
+    };
+  },
+  methods: {
+    checkAll(checked) {
+      this.agreeall = checked;
+      this.agree1 = checked;
+      this.agree2 = checked;
+    },
+    check(num) {
+      if (num === 1) {
+        this.agree1 = !this.agree1;
+        if (this.agree1 && this.agree2) {
+          this.agreeall = true;
+        }
+      } else if (num === 2) {
+        this.agree2 = !this.agree2;
+        if (this.agree1 && this.agree2) {
+          this.agreeall = true;
+        }
+      }
+      if (!this.agree1 || !this.agree2) this.agreeall = false;
+    },
+    nextpage() {
+      this.$emit("agreeall", this.agreeall);
+    },
+  },
+};
+</script>
+
+<style scoped>
+@font-face {
+  font-family: "NEXON Lv1 Gothic OTF Bold";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF Bold.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "NEXON Lv1 Gothic OTF";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+#terms {
+  padding: 20px;
+}
+#terms h2 {
+  text-align: center;
+  font-family: "NEXON Lv1 Gothic OTF";
+  margin-bottom: 20px;
+}
+
+.term {
+  width: 570px;
+  height: 400px;
+  max-height: 400px;
+  overflow-y: auto;
+  margin: 20px auto;
+  background-color: #f8f8f8;
+}
+
+.title {
+  width: 570px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title p {
+  font-size: 18px;
+  font-family: "NEXON Lv1 Gothic OTF";
+  margin-bottom: 0px;
+}
+
+.term p,
+h5 {
+  font-family: "NEXON Lv1 Gothic OTF";
+  font-size: 13px;
+}
+.agree {
+  margin: 0 auto;
+  text-align: center;
+}
+.btn {
+  margin: 0 0 0 1330px;
+}
+input[type="checkbox"] {
+  zoom: 1.2;
+}
+label {
+  font-family: "NEXON Lv1 Gothic OTF";
+}
+#agree,
+.all {
+  margin: 20px auto;
+}
+.term1 {
+  margin-bottom: 20px;
+}
+</style>
