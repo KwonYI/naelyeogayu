dc3166cdadce5ca3979237704dd3930c6858a201
천창민 <chunawoos@hanmail.net>
2021-05-10 05:22:48 +0000

    #S04P31A103-152 상세페이지 구현

---
+++ frontend/src/components/product/detail/DetailDescript.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="productDetailDescript">
+    <div class="productDetailDescriptHeader">상세정보</div>
+    <div class="productDetailDescriptBody">{{ item.product.descript }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
+
+<style></style>
