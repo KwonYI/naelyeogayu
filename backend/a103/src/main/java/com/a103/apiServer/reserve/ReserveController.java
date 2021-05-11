5eda9da0047372e613886be39d16830d0e4d757a
KwonYI <kkwinwin95@naver.com>
2021-05-11 11:43:43 +0900

    #S04P31A103-182 refactor : 상품 정보 중복 제거

--- backend/a103/src/main/java/com/a103/apiServer/reserve/ReserveController.java
+++ backend/a103/src/main/java/com/a103/apiServer/reserve/ReserveController.java
@@ -57,7 +57,9 @@ public class ReserveController {
 					Map info = new HashMap<>();
 					ProductDetail product = productService.getProductDetail(reserve.getProduct(), now);
 					info.put("reserve", reserve);
-					info.put("product", product);
+					info.put("productCurPrice ", product.getCurPrice());
+					info.put("productCurDiscountRate  ", product.getDiscountRate());
+					info.put("productCurDday  ", product.getDDay());
 					data.add(info);
 				}
 				result.put("success", "success");
