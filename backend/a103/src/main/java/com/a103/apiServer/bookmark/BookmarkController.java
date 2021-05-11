5b23c687b859b44480056eeb867716940c144338
김명규 <figtoo@naver.com>
2021-05-11 14:36:35 +0900

    #S04P31A103-169 fix : 북마크 기능 수정

--- backend/a103/src/main/java/com/a103/apiServer/bookmark/BookmarkController.java
+++ backend/a103/src/main/java/com/a103/apiServer/bookmark/BookmarkController.java
@@ -1,5 +1,7 @@
 package com.a103.apiServer.bookmark;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +21,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.a103.apiServer.model.Bookmark;
+import com.a103.apiServer.model.Product;
+import com.a103.apiServer.model.ProductDetail;
+import com.a103.apiServer.product.ProductDao;
+import com.a103.apiServer.product.ProductService;
 
 @RestController
 @RequestMapping("/bookmark")
@@ -26,6 +32,12 @@ public class BookmarkController {
 
 	@Autowired
 	private BookmarkDao bookmarkDao;
+
+	@Autowired
+	ProductDao productDao;
+
+	@Autowired
+	private ProductService productService;
 
 	private static final Logger logger = LoggerFactory.getLogger(BookmarkController.class);
 
@@ -65,8 +77,16 @@ public class BookmarkController {
 			List<Bookmark> bookmarkList = bookmarkDao.findListBookmarkByMemberId(memberId);
 
 			if (bookmarkList.size() != 0) {
+				List<ProductDetail> productList = new ArrayList<>();
+				LocalDateTime now = LocalDateTime.now();
+
+				for (Bookmark mark : bookmarkList) {
+					productList
+							.add(productService.getProductDetail(productDao.findProductById(mark.getProductId()), now));
+				}
+
 				result.put("success", "success");
-				result.put("data", bookmarkList);
+				result.put("data", productList);
 				entity = new ResponseEntity(result, HttpStatus.OK);
 			} else {
 				result.put("success", "fail");
@@ -82,13 +102,39 @@ public class BookmarkController {
 		return entity;
 	}
 
-	@DeleteMapping(value = "")
-	public ResponseEntity deleteBookmark(@RequestHeader(value = "id") long bookmarkId) {
+	@GetMapping(value = "/list/{member_id}/{product_id}")
+	public ResponseEntity checkBookmark(@PathVariable("member_id") long memberId,
+			@PathVariable("product_id") long productId) {
 		ResponseEntity entity = null;
 		Map result = new HashMap();
 
 		try {
-			bookmarkDao.deleteById(bookmarkId);
+			Bookmark findBookmark = bookmarkDao.findBookmarkByMemberIdAndProductId(memberId, productId);
+
+			if (findBookmark != null) {
+				result.put("success", "success");
+				entity = new ResponseEntity(result, HttpStatus.OK);
+			} else {
+				result.put("success", "fail");
+				entity = new ResponseEntity(result, HttpStatus.BAD_REQUEST);
+			}
+
+		} catch (Exception e) {
+			logger.error("error", e);
+			result.put("success", "error");
+			entity = new ResponseEntity(result, HttpStatus.BAD_REQUEST);
+		}
+
+		return entity;
+	}
+
+	@DeleteMapping(value = "")
+	public ResponseEntity deleteBookmark(@RequestBody Bookmark bookmark) {
+		ResponseEntity entity = null;
+		Map result = new HashMap();
+
+		try {
+			bookmarkDao.deleteById(bookmark.getId());
 			result.put("success", "success");
 			entity = new ResponseEntity(result, HttpStatus.OK);
 		} catch (Exception e) {
