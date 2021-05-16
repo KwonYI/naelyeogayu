260196e38dbefb1fa5eca30e7cf0bc6bff2db345
하태민 <haruto4380@gmail.com>
2021-05-17 08:41:16 +0900

    #S04P31A103-188 #S04P31A103-164 리뷰 컴포넌트 구현 및 리뷰 API 수정

--- backend/a103/src/main/java/com/a103/apiServer/model/Review.java
+++ backend/a103/src/main/java/com/a103/apiServer/model/Review.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 
 import lombok.AllArgsConstructor;
@@ -22,8 +24,9 @@ public class Review {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	@Column(name = "member_id")
-	private long memberId;
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
 
 	@Column(name = "product_id")
 	private long productId;
